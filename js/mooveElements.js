ca29bfd359ba2ff1467137df11a6c8ea8e027bbb
grischat <kosolapg2@gmail.com>
2023-12-26 20:37:20 +0100

    -AND ONE MORE!1121!1

--- js/mooveElements.js
+++ js/mooveElements.js
@@ -44,11 +44,11 @@ document.addEventListener("DOMContentLoaded", function () {
   function changeImage(windowSize) {
     const backgroundImg = document.querySelector(".mobile__background-image");
     if (windowSize < 768) {
-      backgroundImg.src = "../pod-landing/assets/mobile/image-host.jpg";
+      backgroundImg.src = "../assets/mobile/image-host.jpg";
     } else if (windowSize >= 768 && windowSize < 1440) {
-      backgroundImg.src = "../pod-landing/assets/tablet/image-host.jpg";
+      backgroundImg.src = "../assets/tablet/image-host.jpg";
     } else {
-      backgroundImg.src = "../pod-landing/assets/desktop/image-host.jpg";
+      backgroundImg.src = "../assets/desktop/image-host.jpg";
     }
   }
 });
